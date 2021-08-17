d7c43d589506f4c31865165f420b6913370e6027
Robert Barlin <robert@accessy.se>
2021-08-17 07:14:49 +0200

    Show table for each binary digit equivilant

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,58 @@
-use std::error::Error;
+use std::{error::Error, io::Write, iter};
+
+use prettytable::{Cell, Row, Table};
 
 fn main() {
     let matches = clap::App::new("b2x")
         .arg(clap::Arg::new("input").required(true))
         .get_matches();
 
-    let sum: i64 = matches
+    let values: Vec<_> = matches
         .value_of("input")
         .unwrap()
         .chars()
         .rev()
-        .map(char_to_i64)
+        .map(char_to_u64)
         .enumerate()
-        .map(|(i, d)| adjust_to_nth_place(i, d.unwrap()))
-        .sum();
+        .map(|(i, d)| (i, adjust_to_nth_place(i, d.unwrap())))
+        .collect::<Vec<(usize, u64)>>()
+        .into_iter()
+        .rev()
+        .collect();
 
-    println!("{}", sum);
+    print_values(&values);
+    println!("Equals: {}\n", values.iter().map(|(_, v)| v).sum::<u64>());
 }
 
-fn adjust_to_nth_place(nth_place: usize, number: i64) -> i64 {
+fn print_values(values: &[(usize, u64)]) {
+    let mut table = Table::new();
+
+    let header = Row::new(
+        values
+            .iter()
+            .map(|(h, _)| h.to_string())
+            .map(|s| Cell::new(&s))
+            .collect(),
+    );
+    table.add_row(header);
+
+    let values = Row::new(
+        values
+            .iter()
+            .map(|(_, v)| v.to_string())
+            .map(|s| Cell::new(&s))
+            .collect(),
+    );
+    table.add_row(values);
+
+    table.printstd();
+}
+
+fn adjust_to_nth_place(nth_place: usize, number: u64) -> u64 {
     number << nth_place
 }
 
-fn char_to_i64(c: char) -> Result<i64, Box<dyn Error>> {
+fn char_to_u64(c: char) -> Result<u64, Box<dyn Error>> {
     match c {
         '0' => Ok(0),
         '1' => Ok(1),
