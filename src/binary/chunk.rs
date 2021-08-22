691f5e7c23b598692c23be9006713bf9a4b1336c
Robert Barlin <robert@accessy.se>
2021-08-22 13:24:40 +0200

    Parse signed for 16,32 and 64 for both ends

--- src/binary/chunk.rs
+++ src/binary/chunk.rs
@@ -34,7 +34,7 @@ pub fn as_big_endian_u8(chunk: &[u8]) -> u8 {
         .sum()
 }
 
-// Big Endian Signed Deciaml
+// Big Endian Signed Decimal
 pub fn as_big_endian_i8(chunk: &[u8]) -> i8 {
     let signed = *chunk.first().unwrap();
     let tail = &chunk[1..];
@@ -53,7 +53,61 @@ pub fn as_big_endian_i8(chunk: &[u8]) -> i8 {
     as_big_endian_u8(chunk) as i8
 }
 
-// Big Endian Signed Deciaml
+pub fn as_big_endian_i16(chunk: &[u8]) -> i16 {
+    let signed = *chunk.first().unwrap();
+    let tail = &chunk[1..];
+    if signed == 1 {
+        let mut inverted = tail
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+        let mut new_inverted = vec![0];
+        new_inverted.append(&mut inverted);
+
+        let number = as_big_endian_u16(&new_inverted) as i16;
+        return (number * -1) - 1;
+    }
+
+    as_big_endian_u16(chunk) as i16
+}
+
+pub fn as_big_endian_i32(chunk: &[u8]) -> i32 {
+    let signed = *chunk.first().unwrap();
+    let tail = &chunk[1..];
+    if signed == 1 {
+        let mut inverted = tail
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+        let mut new_inverted = vec![0];
+        new_inverted.append(&mut inverted);
+
+        let number = as_big_endian_u32(&new_inverted) as i32;
+        return (number * -1) - 1;
+    }
+
+    as_big_endian_u32(chunk) as i32
+}
+
+pub fn as_big_endian_i64(chunk: &[u8]) -> i64 {
+    let signed = *chunk.first().unwrap();
+    let tail = &chunk[1..];
+    if signed == 1 {
+        let mut inverted = tail
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+        let mut new_inverted = vec![0];
+        new_inverted.append(&mut inverted);
+
+        let number = as_big_endian_u64(&new_inverted) as i64;
+        return (number * -1) - 1;
+    }
+
+    as_big_endian_u64(chunk) as i64
+}
+
+// Big Endian Signed Decimal
 pub fn as_little_endian_i8(chunk: &[u8]) -> i8 {
     let signed = *chunk.last().unwrap();
     let init = &chunk[0..chunk.len() - 1];
@@ -68,6 +122,54 @@ pub fn as_little_endian_i8(chunk: &[u8]) -> i8 {
     }
 
     as_little_endian_u8(init) as i8
+}
+
+pub fn as_little_endian_i16(chunk: &[u8]) -> i16 {
+    let signed = *chunk.last().unwrap();
+    let init = &chunk[0..chunk.len() - 1];
+    if signed == 1 {
+        let inverted = init
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+
+        let number = as_little_endian_u16(&inverted) as i16;
+        return (number * -1) - 1;
+    }
+
+    as_little_endian_u16(init) as i16
+}
+
+pub fn as_little_endian_i32(chunk: &[u8]) -> i32 {
+    let signed = *chunk.last().unwrap();
+    let init = &chunk[0..chunk.len() - 1];
+    if signed == 1 {
+        let inverted = init
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+
+        let number = as_little_endian_u32(&inverted) as i32;
+        return (number * -1) - 1;
+    }
+
+    as_little_endian_u32(init) as i32
+}
+
+pub fn as_little_endian_i64(chunk: &[u8]) -> i64 {
+    let signed = *chunk.last().unwrap();
+    let init = &chunk[0..chunk.len() - 1];
+    if signed == 1 {
+        let inverted = init
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+
+        let number = as_little_endian_u64(&inverted) as i64;
+        return (number * -1) - 1;
+    }
+
+    as_little_endian_u64(init) as i64
 }
 
 // Little Endian Unsigned Decimal
@@ -242,6 +344,97 @@ mod tests {
     }
 
     #[test]
+    fn as_big_endian_i16() {
+        let input = [[1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+            .iter()
+            .flatten()
+            .map(|v| *v)
+            .collect::<Vec<u8>>();
+
+        let actual = super::as_big_endian_i16(&input);
+        assert_eq!(i16::min_value(), actual);
+
+        let input = [[0, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1]]
+            .iter()
+            .flatten()
+            .map(|v| *v)
+            .collect::<Vec<u8>>();
+
+        let actual = super::as_big_endian_i16(&input);
+        assert_eq!(i16::max_value(), actual);
+    }
+
+    #[test]
+    fn as_big_endian_i32() {
+        let input = [
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_big_endian_i32(&input);
+        assert_eq!(i32::min_value(), actual);
+
+        let input = [
+            [0, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_big_endian_i32(&input);
+        assert_eq!(i32::max_value(), actual);
+    }
+
+    #[test]
+    fn as_big_endian_i64() {
+        let input = [
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_big_endian_i64(&input);
+        assert_eq!(i64::min_value(), actual);
+
+        let input = [
+            [0, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_big_endian_i64(&input);
+        assert_eq!(i64::max_value(), actual);
+    }
+
+    #[test]
     fn as_little_endian_i8() {
         let input = &[0, 0, 0, 0, 0, 0, 0, 1];
         let actual = super::as_little_endian_i8(input);
@@ -258,5 +451,96 @@ mod tests {
         let input = &[1, 1, 1, 1, 1, 1, 1, 0];
         let actual = super::as_little_endian_i8(input);
         assert_eq!(127, actual);
+    }
+
+    #[test]
+    fn as_little_endian_i16() {
+        let input = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1]]
+            .iter()
+            .flatten()
+            .map(|v| *v)
+            .collect::<Vec<u8>>();
+
+        let actual = super::as_little_endian_i16(&input);
+        assert_eq!(i16::min_value(), actual);
+
+        let input = [[1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 0]]
+            .iter()
+            .flatten()
+            .map(|v| *v)
+            .collect::<Vec<u8>>();
+
+        let actual = super::as_little_endian_i16(&input);
+        assert_eq!(i16::max_value(), actual);
+    }
+
+    #[test]
+    fn as_little_endian_i32() {
+        let input = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_little_endian_i32(&input);
+        assert_eq!(i32::min_value(), actual);
+
+        let input = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 0],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_little_endian_i32(&input);
+        assert_eq!(i32::max_value(), actual);
+    }
+
+    #[test]
+    fn as_little_endian_i64() {
+        let input = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_little_endian_i64(&input);
+        assert_eq!(i64::min_value(), actual);
+
+        let input = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 0],
+        ]
+        .iter()
+        .flatten()
+        .map(|v| *v)
+        .collect::<Vec<u8>>();
+
+        let actual = super::as_little_endian_i64(&input);
+        assert_eq!(i64::max_value(), actual);
     }
 }
