c06a9c5ca6bbc5add2701ea72b67d14340aab90c
Robert Barlin <robert@accessy.se>
2021-08-22 12:54:08 +0200

    Parse signed decimal

--- src/binary/chunk.rs
+++ src/binary/chunk.rs
@@ -1,3 +1,4 @@
+// Big Endian Unsigned Decimal
 pub fn as_big_endian_u64(chunk: &[u8]) -> u64 {
     chunk
         .iter()
@@ -33,6 +34,26 @@ pub fn as_big_endian_u8(chunk: &[u8]) -> u8 {
         .sum()
 }
 
+// Big Endian Signed Deciaml
+pub fn as_big_endian_i8(chunk: &[u8]) -> i8 {
+    let signed = *chunk.first().unwrap();
+    let tail = &chunk[1..];
+    if signed == 1 {
+        let mut inverted = tail
+            .iter()
+            .map(|v| if *v == 0 { 1 } else { 0 })
+            .collect::<Vec<_>>();
+        let mut new_inverted = vec![0];
+        new_inverted.append(&mut inverted);
+
+        let number = as_big_endian_u8(&new_inverted) as i8;
+        return (number * -1) - 1;
+    }
+
+    as_big_endian_u8(chunk) as i8
+}
+
+// Little Endian Unsigned Decimal
 pub fn as_little_endian_u8(chunk: &[u8]) -> u8 {
     chunk
         .iter()
@@ -182,5 +203,24 @@ mod tests {
 
         let actual = super::as_little_endian_u64(&input);
         assert_eq!(0b1 << 63, actual);
+    }
+
+    #[test]
+    fn as_big_endian_i8() {
+        let input = &[1, 0, 0, 0, 0, 0, 0, 0];
+        let actual = super::as_big_endian_i8(input);
+        assert_eq!(-128, actual);
+
+        let input = &[1, 0, 0, 0, 0, 0, 0, 1];
+        let actual = super::as_big_endian_i8(input);
+        assert_eq!(-127, actual);
+
+        let input = &[0, 0, 0, 0, 0, 0, 0, 1];
+        let actual = super::as_big_endian_i8(input);
+        assert_eq!(1, actual);
+
+        let input = &[0, 1, 1, 1, 1, 1, 1, 1];
+        let actual = super::as_big_endian_i8(input);
+        assert_eq!(127, actual);
     }
 }
