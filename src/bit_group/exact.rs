140f40482401f4338ca15393187597ba1b842569
fabrlyn <robert.barlin@gmail.com>
2022-04-10 21:41:31 +0200

    Remove redundant code from exact bit group

--- src/bit_group/exact.rs
+++ src/bit_group/exact.rs
@@ -1,6 +1,10 @@
 use std::marker::PhantomData;
 
-use super::common::{DecimalConverter, DecimalConverterError, DefaultBitGroup, FromBinary};
+use crate::{binary, bit_size::BitSize, from_binary::FromBinaryString};
+
+use super::common::{
+    BigEndian, DecimalConverter, DecimalConverterError, DefaultBitGroup, FromBinary, LittleEndian,
+};
 
 // Structs
 
@@ -9,276 +13,77 @@ pub struct ExactBitGroup<T>(PhantomData<T>);
 // Impls
 
 impl<'a, E> DecimalConverter<'a, DefaultBitGroup, E> {
-    pub fn u8(self) -> DecimalConverter<'a, ExactBitGroup<u8>, E> {
+    fn into_output<N>(self) -> DecimalConverter<'a, ExactBitGroup<N>, E> {
         DecimalConverter {
             input: self.input,
             output_marker: PhantomData,
             endian_marker: self.endian_marker,
         }
+    }
+
+    pub fn u8(self) -> DecimalConverter<'a, ExactBitGroup<u8>, E> {
+        self.into_output()
     }
 
     pub fn i8(self) -> DecimalConverter<'a, ExactBitGroup<i8>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 
     pub fn u16(self) -> DecimalConverter<'a, ExactBitGroup<u16>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
     pub fn i16(self) -> DecimalConverter<'a, ExactBitGroup<i16>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
     pub fn u32(self) -> DecimalConverter<'a, ExactBitGroup<u32>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 
     pub fn i32(self) -> DecimalConverter<'a, ExactBitGroup<i32>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 
     pub fn u64(self) -> DecimalConverter<'a, ExactBitGroup<u64>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 
     pub fn i64(self) -> DecimalConverter<'a, ExactBitGroup<i64>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 
     pub fn u128(self) -> DecimalConverter<'a, ExactBitGroup<u128>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 
     pub fn i128(self) -> DecimalConverter<'a, ExactBitGroup<i128>, E> {
-        DecimalConverter {
-            input: self.input,
-            output_marker: PhantomData,
-            endian_marker: self.endian_marker,
-        }
+        self.into_output()
     }
 }
 
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<i8>, E> {
-    type Output = Result<Vec<i8>, DecimalConverterError>;
+impl<'a, T> FromBinary for DecimalConverter<'a, ExactBitGroup<T>, LittleEndian>
+where
+    T: BitSize + FromBinaryString,
+{
+    type Output = Result<Vec<<T as FromBinaryString>::Output>, DecimalConverterError>;
 
     fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(8)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| {
-                u8::from_str_radix(&s, 2)
-                    .map_err(|_| DecimalConverterError::ParseError(s))
-                    .map(|i| i as i8)
-            })
-            .collect()
+        binary::little_endian_to_decimal::<T>(self.input).map_err(DecimalConverterError::ParseError)
     }
 }
 
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<u8>, E> {
-    type Output = Result<Vec<u8>, DecimalConverterError>;
+impl<'a, T> FromBinary for DecimalConverter<'a, ExactBitGroup<T>, BigEndian>
+where
+    T: BitSize + FromBinaryString,
+{
+    type Output = Result<Vec<<T as FromBinaryString>::Output>, DecimalConverterError>;
 
     fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(8)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| u8::from_str_radix(&s, 2).map_err(|_| DecimalConverterError::ParseError(s)))
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<u16>, E> {
-    type Output = Result<Vec<u16>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(16)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| u16::from_str_radix(&s, 2).map_err(|_| DecimalConverterError::ParseError(s)))
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<i16>, E> {
-    type Output = Result<Vec<i16>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(16)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| {
-                u16::from_str_radix(&s, 2)
-                    .map_err(|_| DecimalConverterError::ParseError(s))
-                    .map(|i| i as i16)
-            })
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<u32>, E> {
-    type Output = Result<Vec<u32>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(32)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| u32::from_str_radix(&s, 2).map_err(|_| DecimalConverterError::ParseError(s)))
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<i32>, E> {
-    type Output = Result<Vec<i32>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(32)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| {
-                u32::from_str_radix(&s, 2)
-                    .map_err(|_| DecimalConverterError::ParseError(s))
-                    .map(|i| i as i32)
-            })
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<u64>, E> {
-    type Output = Result<Vec<u64>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(64)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| u64::from_str_radix(&s, 2).map_err(|_| DecimalConverterError::ParseError(s)))
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<i64>, E> {
-    type Output = Result<Vec<i64>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(64)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| {
-                u64::from_str_radix(&s, 2)
-                    .map_err(|_| DecimalConverterError::ParseError(s))
-                    .map(|i| i as i64)
-            })
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<u128>, E> {
-    type Output = Result<Vec<u128>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(128)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| u128::from_str_radix(&s, 2).map_err(|_| DecimalConverterError::ParseError(s)))
-            .collect()
-    }
-}
-
-impl<'a, E> FromBinary for DecimalConverter<'a, ExactBitGroup<i128>, E> {
-    type Output = Result<Vec<i128>, DecimalConverterError>;
-
-    fn from_binary(&self) -> Self::Output {
-        let input: Vec<_> = self.input.chars().map(|c| c as u8).collect();
-        input
-            .as_slice()
-            .chunks(128)
-            .flat_map(|s| {
-                String::from_utf8(s.to_vec())
-                    .map_err(|_| DecimalConverterError::ParseError(format!("{:?}", s)))
-            })
-            .map(|s| {
-                u128::from_str_radix(&s, 2)
-                    .map_err(|_| DecimalConverterError::ParseError(s))
-                    .map(|i| i as i128)
-            })
-            .collect()
+        binary::little_endian_to_decimal::<T>(self.input).map_err(DecimalConverterError::ParseError)
     }
 }
 
 // Tests
+
 #[cfg(test)]
 mod tests {
     use crate::bit_group::common::DecimalConverterExt;
